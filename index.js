0c02cbeebd06fcbc3e34651f7a9e13bcd256d479
Francisco Estevez <franciscoestevez987@gmail.com>
2025-10-24 22:20:06 -0300

    POST products <item> <price> <category>

--- index.js
+++ index.js
@@ -21,3 +21,27 @@ if (method === "GET" && resource.startsWith("products/")) {
     }
 }
 
+
+// npm start POST products Buzo 15990 abrigo
+if (method === "POST" && resource === "products") {
+    let [, , title, price, category = "generico"] = process.argv;
+    price = parseFloat(price);
+    category = category.toLowerCase();
+
+    if (!isNaN(price) || price <= 0) {
+        const product = {
+            title: title,
+            price: price,
+            category: category
+        };
+        fetch('https://fakestoreapi.com/products', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(product)
+        })
+            .then(response => response.json())
+            .then(data => console.log(data));
+    } else {
+        console.log("Error: El precio debe ser un valor positivo.");
+    }
+}
