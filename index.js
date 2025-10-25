7309e9e79188df342769c200a23814cbd7cec2f4
Francisco Estevez <franciscoestevez987@gmail.com>
2025-10-24 21:08:34 -0300

    GET productos

---
+++ index.js
@@ -0,0 +1,9 @@
+const [, , method, resource] = process.argv
+method = method.toUpperCase()
+resource = resource.toLowerCase()
+
+if (method === "GET" && resource === "products") {
+    fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
