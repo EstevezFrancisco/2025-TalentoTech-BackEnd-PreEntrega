67fbfe57c4391cf99dc9d2fe46925b500fddb931
Francisco Estevez <franciscoestevez987@gmail.com>
2025-10-24 22:19:08 -0300

    GET products/id

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const [, , method, resource] = process.argv
-method = method.toUpperCase()
-resource = resource.toLowerCase()
+const method = process.argv[2].toUpperCase()
+const resource = process.argv[3].toLowerCase()
 
+// npm start GET products
 if (method === "GET" && resource === "products") {
     fetch('https://fakestoreapi.com/products')
         .then(response => response.json())
         .then(data => console.log(data));
 }
+
+
+// npm start GET products/4
+if (method === "GET" && resource.startsWith("products/")) {
+    const productId = parseInt(resource.split("/")[1]);
+    if (!productId || productId < 1) {
+        console.log("Error: Debe ingresar un número positivo.");
+    } else {
+        fetch(`https://fakestoreapi.com/products/${productId}`)
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
+}
+
