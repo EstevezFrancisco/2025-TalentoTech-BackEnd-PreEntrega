2e9d60b520db65c57512753303aaffa1f8a97ba2
Francisco Estevez <franciscoestevez987@gmail.com>
2025-10-24 22:20:56 -0300

    DELETE products/id

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const method = process.argv[2].toUpperCase()
 const resource = process.argv[3].toLowerCase()
 
+
 // npm start GET products
 if (method === "GET" && resource === "products") {
     fetch('https://fakestoreapi.com/products')
         .then(response => response.json())
         .then(data => console.log(data));
 }
-
 
 // npm start GET products/4
 if (method === "GET" && resource.startsWith("products/")) {
@@ -20,7 +20,6 @@ if (method === "GET" && resource.startsWith("products/")) {
             .then(data => console.log(data));
     }
 }
-
 
 // npm start POST products Buzo 15990 abrigo
 if (method === "POST" && resource === "products") {
@@ -43,5 +42,19 @@ if (method === "POST" && resource === "products") {
             .then(data => console.log(data));
     } else {
         console.log("Error: El precio debe ser un valor positivo.");
+    }
+}
+
+// npm start DELETE products/4
+if (method === "DELETE" && resource.startsWith("products/")) {
+    const productId = parseInt(resource.split("/")[1]);
+    if (!productId || productId < 1) {
+        console.log("Error: Debe ingresar un número positivo.");
+    } else {
+        fetch('https://fakestoreapi.com/products/1', {
+            method: 'DELETE'
+        })
+            .then(response => response.json())
+            .then(data => console.log(data));
     }
 }
